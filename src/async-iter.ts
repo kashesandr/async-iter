e0bc7f9237aba200f28a430b8aa1082f64839ffe
Alexander Kasheverov <alexandresbox@gmail.com>
2023-10-17 12:41:36 +0800

    update

--- src/async-iter.ts
+++ src/async-iter.ts
@@ -6,56 +6,55 @@ export class AsyncIter<T> {
         this.asyncIter = asyncIter;
     }
 
-    async *[Symbol.asyncIterator]() {
+    async *[Symbol.asyncIterator](): AsyncIterator<T> {
         // @ts-ignore
         yield * this.asyncIter;
     }
 
     filter(filterFn: (item: T) => boolean): AsyncIter<T> {
-        const {asyncIter} = this;
-        const newIter = (async function* (){
+        const { asyncIter } = this;
+        const newIter = (async function* () {
             // @ts-ignore
             for await (const item of asyncIter) {
                 if (filterFn(item)) {
                     yield item;
                 }
             }
-        })()
+        })();
         return new AsyncIter<T>(newIter);
     }
 
     map(mapFn: (item: T) => T): AsyncIter<T> {
-        const {asyncIter} = this;
-        const newIter = (async function* (){
+        const { asyncIter } = this;
+        const newIter = (async function* () {
             // @ts-ignore
             for await (const item of asyncIter) {
                 yield mapFn(item);
             }
-        })()
+        })();
         return new AsyncIter<T>(newIter);
     }
 
     mapAsync(mapFnAsync: (item: T) => Promise<T>): AsyncIter<T> {
-        const {asyncIter} = this;
-        const newIter = (async function* (){
+        const { asyncIter } = this;
+        const newIter = (async function* () {
             // @ts-ignore
             for await (const item of asyncIter) {
                 yield await mapFnAsync(item);
             }
-        })()
+        })();
         return new AsyncIter<T>(newIter);
     }
 
     enumerate(): AsyncIter<[number, T][]> {
-        const {asyncIter} = this;
-        const newIter = (async function* (){
+        const { asyncIter } = this;
+        const newIter = (async function* () {
             let i = 0;
             // @ts-ignore
             for await (const item of asyncIter) {
                 yield [i++, item];
             }
-        })()
+        })();
         return new AsyncIter(newIter);
     }
-
 }

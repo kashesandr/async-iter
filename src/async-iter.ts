e06f691dd620c057959c261e85386a36ed91608b
Alexander Kasheverov <alexandresbox@gmail.com>
2023-10-18 22:46:05 +0800

    update

--- src/async-iter.ts
+++ src/async-iter.ts
@@ -11,7 +11,7 @@ export class AsyncIter<T> {
         yield * this.asyncIter;
     }
 
-    filter(filterFn: (item: T) => boolean): AsyncIter<T> {
+    filter<K>(filterFn: (item: K) => boolean): AsyncIter<K> {
         const { asyncIter } = this;
         const newIter = (async function* () {
             // @ts-ignore
@@ -21,10 +21,10 @@ export class AsyncIter<T> {
                 }
             }
         })();
-        return new AsyncIter<T>(newIter);
+        return new AsyncIter<K>(newIter);
     }
 
-    map(mapFn: (item: T) => T): AsyncIter<T> {
+    map<K, L>(mapFn: (item: K) => L): AsyncIter<L> {
         const { asyncIter } = this;
         const newIter = (async function* () {
             // @ts-ignore
@@ -32,10 +32,10 @@ export class AsyncIter<T> {
                 yield mapFn(item);
             }
         })();
-        return new AsyncIter<T>(newIter);
+        return new AsyncIter<L>(newIter);
     }
 
-    mapAsync(mapFnAsync: (item: T) => Promise<T>): AsyncIter<T> {
+    mapAsync<K, L>(mapFnAsync: (item: K) => Promise<L>): AsyncIter<L> {
         const { asyncIter } = this;
         const newIter = (async function* () {
             // @ts-ignore
@@ -43,10 +43,10 @@ export class AsyncIter<T> {
                 yield await mapFnAsync(item);
             }
         })();
-        return new AsyncIter<T>(newIter);
+        return new AsyncIter<L>(newIter);
     }
 
-    enumerate(): AsyncIter<[number, T][]> {
+    enumerate<K>(): AsyncIter<[number, K][]> {
         const { asyncIter } = this;
         const newIter = (async function* () {
             let i = 0;
@@ -55,6 +55,6 @@ export class AsyncIter<T> {
                 yield [i++, item];
             }
         })();
-        return new AsyncIter(newIter);
+        return new AsyncIter<[number, K][]>(newIter);
     }
 }
